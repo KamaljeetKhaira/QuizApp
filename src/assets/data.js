89b691cac521a2baf1df22f47d0fbc52e2641ebd
KamaljeetKhaira <144229361+KamaljeetKhaira@users.noreply.github.com>
2024-03-13 02:29:13 -0400

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+        question: "Which device is required for Internet Connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question:"Which continent has highest number of countries?",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "North America",
+        option4: "Africa",
+        ans: 4,
+    },
+    {
+        question: "Junk email is also called",
+        option1: "Spam",
+        option2: "fake",
+        option3: "Archieved",
+        option4: "Bin",
+        ans: 1,
+    },
+    {
+        question: "A computer does not BOOT if it does not have the?",
+        option1: "Application Software",
+        option2: "Internet",
+        option3: "Operating System",
+        option4: "Mouse",
+        ans: 3,
+    },
+    {
+        question: "First page of the website is termed is ?",
+        option1: "Index page",
+        option2: "Home page",
+        option3: "Sitemap",
+        option4: "Pen Drive",
+        ans: 2,
+    },
+
+];
